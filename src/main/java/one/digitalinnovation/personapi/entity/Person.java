c6fe882539f300a442e3e58543827fdfafa35b20
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2021-10-31 20:32:30 -0300

    Person: persistence;

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -1,22 +1,36 @@
 package one.digitalinnovation.personapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false, unique = true)
     private String cpf;
+
     private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @ToString.Exclude
     private List<Phone> phones;
 }
