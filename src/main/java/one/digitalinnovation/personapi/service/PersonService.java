8169f512719711cf140aab704ee245e05bfc37cb
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2021-11-02 13:47:28 -0300

    PersonService

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
