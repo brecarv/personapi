123fb7b939b79ac3f44cc1a53fd1a33b864c9e08
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2021-11-04 15:54:25 -0300

    PersonMapper created; PersonController and PersonService improved with mapstruct use.

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,7 +1,7 @@
 package one.digitalinnovation.personapi.controller;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 }
