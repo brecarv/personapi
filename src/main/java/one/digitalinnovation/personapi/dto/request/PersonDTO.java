42a24bf6930a4c18637bfa942ca5c12df19ee5a1
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2021-11-04 15:52:29 -0300

    PersonDTO and PhoneDTO improved; javax validation and mapstruct added

--- src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/PersonDTO.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -16,14 +20,22 @@ public class PersonDTO {
 
     private Long id;
 
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String firstname;
 
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String lastName;
 
+    @NotEmpty
+    @CPF
     private String cpf;
 
     private String birthDate;
 
+    @Valid
+    @NotEmpty
     private List<PhoneDTO> phones;
 
 }
